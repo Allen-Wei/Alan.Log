0b2efa2a826cdc4f75ae37192a081f22746575b6
Alan <alan.wei@live.com>
2016-02-25 18:35:07 +0800

    update Alan.Log.Bmob

---
+++ Alan.Log.Bmob/LogModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using cn.bmob.io;
+
+namespace Alan.Log.Bmob
+{
+    public class LogModel : BmobTable
+    {
+
+
+        public override string table
+        {
+            get { return "Logs"; }
+        }
+
+        //读字段信息
+        public override void readFields(BmobInput input)
+        {
+            base.readFields(input);
+
+            this.score = input.getInt("score");
+            this.cheatMode = input.getBoolean("cheatMode");
+            this.playerName = input.getString("playerName");
+        }
+
+        //写字段信息
+        public override void write(BmobOutput output, bool all)
+        {
+            base.write(output, all);
+
+            output.Put("score", this.score);
+            output.Put("cheatMode", this.cheatMode);
+            output.Put("playerName", this.playerName);
+        }
+    }
+}
+}
